1151ee62236ca064d73f11760d5cdfbcb1422cc7
Rafael Guerino <rafaelguerino5180@gmail.com>
2026-03-08 16:53:22 -0300

    build: Conecta o banco de dados H2 a minha aplicacao SPRING

--- src/main/java/dev/rafagguerino/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/dev/rafagguerino/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -1,12 +1,14 @@
 package dev.rafagguerino.CadastroDeNinjas.Missoes;
 
 import dev.rafagguerino.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.TableGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,10 +24,13 @@ public class MissaoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_missao")
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
 
+    @Column(name = "dificuldade")
     private String dificuldade;
 
     // @OneToMany: Uma missao pode ter varios ninjas
