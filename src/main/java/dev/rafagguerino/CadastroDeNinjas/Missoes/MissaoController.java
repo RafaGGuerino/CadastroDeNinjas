d07d927680bb599d6625acff6fff545e35b81b86
Rafael Guerino <rafaelguerino5180@gmail.com>
2026-03-10 14:29:08 -0300

    feat: Cria as rotas controller com mock

--- src/main/java/dev/rafagguerino/CadastroDeNinjas/Missoes/MissaoController.java
+++ src/main/java/dev/rafagguerino/CadastroDeNinjas/Missoes/MissaoController.java
@@ -1,12 +1,35 @@
 package dev.rafagguerino.CadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("missoes")
 public class MissaoController {
 
+    // localhost:8080/missoes/*
 
+    // GET --> mandar uma requisição para mostrar as missões
+    @GetMapping("/listar")
+    public String listarMissoes() {
+        return "Lista de missoes";
+    }
+
+    // POST --> mandar uma requisição para criar as missões
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Criando a missao";
+    }
+
+    // PUT --> mandar uma requisição para alterar as missões
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Alterando a missao";
+    }
+
+    // DELETE --> mandar uma requisição para deletar as missões
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Deletando a missao";
+    }
 
 }
