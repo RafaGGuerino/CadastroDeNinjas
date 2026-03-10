928d2435ae4c00a77ccca8f5d3b6fc99aaa2b672
Rafael Guerino <rafaelguerino5180@gmail.com>
2026-03-10 13:35:23 -0300

    feat: Atualiza o Controller com mock de dados

--- src/main/java/dev/rafagguerino/CadastroDeNinjas/Missoes/MissaoController.java
+++ src/main/java/dev/rafagguerino/CadastroDeNinjas/Missoes/MissaoController.java
@@ -1,4 +1,12 @@
 package dev.rafagguerino.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class MissaoController {
+
+
+
 }
--- src/main/java/dev/rafagguerino/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/rafagguerino/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.rafagguerino.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,36 @@ public class NinjaController {
     @GetMapping("/boasVindas")
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota!";
+    }
+
+    // Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado!";
+    }
+
+    // Mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Mostrar todos";
+    }
+
+    //Procurar ninja por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorID() {
+        return  "Mostrar todos por ID";
+    }
+
+    // Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorID() {
+        return  "Alterar ninja por ID";
+    }
+
+    // Deletar ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorID() {
+        return  "Deletar ninja por ID";
     }
 
 }
